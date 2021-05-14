f4809f1d638a9f965e51f8ef4f657391426d20b0
sureshaws9 <sureshpas4009@gmail.com>
2021-05-14 13:48:35 +0530

    square root

---
+++ SquareRoot.java
@@ -0,0 +1,15 @@
+package suripackage;
+
+public class SquareRoot {
+
+	 public static void main (String args[ ])
+	 {
+		 
+		  double x = 5;
+		  double y;
+		  y = Math.sqrt((x));
+		  System.out.println("y = " + y);
+		  
+	 }
+
+}
